4c838359995cce62b8be74c96e82fa3ff8892cea
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-01-13 20:12:58 +0100

    Neuen Frage Typen hinzugefügt: SingleChoiceQuestion

---
+++ CSharpQuiz/Questions/Question.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharpQuiz.Questions;
+
+public abstract class Question
+{
+    protected Question(
+        string text,
+        string typeNote,
+        string hint,
+        int points)
+    {
+        Text = text;
+        TypeNote = typeNote;
+        Hint = hint;
+        Points = points;
+    }
+
+
+    public string Text { get; }
+
+    public string TypeNote { get; }
+    
+    public string Hint { get; }
+
+    public int Points { get; }
+
+
+    public abstract int EvaluatePoints();
+}
---
+++ CSharpQuiz/Questions/SingleChoiceQuestion.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace CSharpQuiz.Questions;
+
+[ObservableObject]
+public partial class SingleChoiceQuestion : Question
+{
+    public SingleChoiceQuestion(
+        string text,
+        string hint,
+        int points,
+        int correctIndex,
+        params string[] choices) : base(text, "Single-Choice-Frage: Wähle einer der folgenden Antworten aus.", hint, points)
+    {
+        CorrectIndex = correctIndex;
+        Choices = new ObservableCollection<string>(choices);
+    }
+
+
+    public int CorrectIndex { get; }
+
+    public ObservableCollection<string> Choices { get; }
+
+    [ObservableProperty]
+    int selectedIndex = 0;
+
+
+    public override int EvaluatePoints()
+    {
+        if (SelectedIndex == CorrectIndex)
+            return Points;
+
+        return 0;
+    }
+}

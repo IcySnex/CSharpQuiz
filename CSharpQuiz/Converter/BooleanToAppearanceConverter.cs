222f4066b14af614950cc6cb09d4e623b88c0fba
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-01-13 20:11:37 +0100

    Converter wurden hinzugefügt

---
+++ CSharpQuiz/Converter/BooleanToAppearanceConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Wpf.Ui.Controls;
+
+namespace CSharpQuiz.Converter;
+
+internal class BooleanToAppearanceConverter : IValueConverter
+{
+    public object Convert(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture) =>
+        (bool)value ? ControlAppearance.Success : ControlAppearance.Danger;
+
+    public object ConvertBack(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture) =>
+        (ControlAppearance)value == ControlAppearance.Success;
+}
---
+++ CSharpQuiz/Converter/BooleanToOpacityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CSharpQuiz.Converter;
+
+internal class BooleanToOpacityConverter : IValueConverter
+{
+    public object Convert(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture) =>
+        (bool)value ? 1 : 0.75;
+
+    public object ConvertBack(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture) =>
+        (double)value == 1;
+}

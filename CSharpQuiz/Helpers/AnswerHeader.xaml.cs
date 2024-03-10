cf144df82cf8588181e1a37fcd2ec7a769bcd8f2
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-03-10 16:23:33 +0100

    Header für Fragen & Antworten in eigenes Control geführt

---
+++ CSharpQuiz/Helpers/AnswerHeader.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CSharpQuiz.Helpers;
+
+public partial class AnswerHeader : UserControl
+{
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(AnswerHeader));
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+
+    public static readonly DependencyProperty PointsProperty =
+        DependencyProperty.Register("Points", typeof(int), typeof(AnswerHeader));
+
+    public int Points
+    {
+        get => (int)GetValue(PointsProperty);
+        set => SetValue(PointsProperty, value);
+    }
+
+
+    public static readonly DependencyProperty ReachedPointsProperty =
+            DependencyProperty.Register("ReachedPoints", typeof(int), typeof(AnswerHeader));
+
+    public int ReachedPoints
+    {
+        get => (int)GetValue(ReachedPointsProperty);
+        set => SetValue(ReachedPointsProperty, value);
+    }
+
+
+    public static readonly DependencyProperty TypeAnswerNoteProperty =
+        DependencyProperty.Register("TypeAnswerNote", typeof(string), typeof(AnswerHeader));
+
+    public string TypeAnswerNote
+    {
+        get => (string)GetValue(TypeAnswerNoteProperty);
+        set => SetValue(TypeAnswerNoteProperty, value);
+    }
+
+
+    public AnswerHeader()
+    {
+        InitializeComponent();
+    }
+}

65c5ad280df085edb96bfb8e2cc7c5aecb819e20
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-03-16 15:44:32 +0100

    Unnötige doppelte Binding bei Fragen & Antworten behoben

--- CSharpQuiz/Helpers/QuestionHeader.xaml.cs
+++ CSharpQuiz/Helpers/QuestionHeader.xaml.cs
@@ -1,40 +1,9 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace CSharpQuiz.Helpers;
 
 public partial class QuestionHeader : UserControl
 {
-    public static readonly DependencyProperty TextProperty =
-        DependencyProperty.Register("Text", typeof(string), typeof(QuestionHeader));
-
-    public string Text
-    {
-        get => (string)GetValue(TextProperty);
-        set => SetValue(TextProperty, value);
-    }
-
-
-    public static readonly DependencyProperty PointsProperty =
-        DependencyProperty.Register("Points", typeof(int), typeof(QuestionHeader));
-
-    public int Points
-    {
-        get => (int)GetValue(PointsProperty);
-        set => SetValue(PointsProperty, value);
-    }
-
-
-    public static readonly DependencyProperty TypeNoteProperty =
-        DependencyProperty.Register("TypeNote", typeof(string), typeof(QuestionHeader));
-
-    public string TypeNote
-    {
-        get => (string)GetValue(TypeNoteProperty);
-        set => SetValue(TypeNoteProperty, value);
-    }
-
-
     public QuestionHeader()
     {
         InitializeComponent();
